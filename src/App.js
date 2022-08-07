cf7f3e5d9e0e6e52dfde0b2066d0df4afa69bbfc
Kevin Shank <Kevin_Shank@yahoo.com>
2022-08-07 13:46:27 -0400

    Get rid of scroll trigger proxy

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import { Grid } from "@material-ui/core"
 import { gsap } from "gsap"
 import { ScrollTrigger } from "gsap/ScrollTrigger"
 import { TextPlugin } from "gsap/TextPlugin"
-import { LocomotiveScrollProvider } from 'react-locomotive-scroll'
-import 'locomotive-scroll/dist/locomotive-scroll.css'
 import Home from "./components/home/Home"
 import ScrollTriggerProxy from "./components/ScrollTriggerProxy"
 import Projects from "./components/projects/Projects"
@@ -16,26 +14,25 @@ import Contact from "./components/contact/Contact"
 gsap.registerPlugin(ScrollTrigger, TextPlugin)
 
 function App() {
-
   const containerRef = useRef(null)
 
   return (
-<LocomotiveScrollProvider
-        options={{
-          smooth: true,
-          multiplier: 1,
-          class: "is-reveal",
-        }}
-        watch={
-          [
-            //..all the dependencies you want to watch to update the scroll.
-            //  Basicaly, you would want to watch page/location changes
-            //  For exemple, on Next.js you would want to watch properties like `router.asPath` (you may want to add more criterias if the instance should be update on locations with query parameters)
-          ]
-        }
-        containerRef={containerRef}
-      >
-<ScrollTriggerProxy />
+    <LocomotiveScrollProvider
+      options={{
+        smooth: true,
+        multiplier: 1,
+        class: "is-reveal",
+      }}
+      watch={
+        [
+          //..all the dependencies you want to watch to update the scroll.
+          //  Basicaly, you would want to watch page/location changes
+          //  For exemple, on Next.js you would want to watch properties like `router.asPath` (you may want to add more criterias if the instance should be update on locations with query parameters)
+        ]
+      }
+      containerRef={containerRef}
+    >
+      <ScrollTriggerProxy />
       <div className="App" data-scroll-container ref={containerRef}>
         <Grid
           container
@@ -51,7 +48,7 @@ function App() {
           <Contact />
         </Grid>
       </div>
-      </LocomotiveScrollProvider>
+    </LocomotiveScrollProvider>
   )
 }
 
