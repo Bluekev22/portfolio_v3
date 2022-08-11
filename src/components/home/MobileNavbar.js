3d3fb91a9fd1842512ba4502fb7d6b1713392552
Kevin Shank <kevin_shank@yahoo.com>
2022-08-11 18:52:45 -0400

    Create logic for bottom nav state

--- src/components/home/MobileNavbar.js
+++ src/components/home/MobileNavbar.js
@@ -1,102 +1,294 @@
-import React from "react"
+import React, { useRef, useState } from "react"
 import { Grid } from "@material-ui/core"
 import { makeStyles } from "@material-ui/core/styles"
+import { gsap } from "gsap"
 import {
   FaAddressCard,
   FaProjectDiagram,
   FaBrain,
   FaEnvelope,
 } from "react-icons/fa"
-import { BiArrowToTop } from "react-icons/bi"
 
 const useStyles = makeStyles((theme) => ({
-  linkContainer: {
+  root: {
     position: "fixed",
     bottom: 0,
-    padding: "10px 10px 5px 10px",
+    width: "100%",
+    padding: "10px",
     backgroundColor: "white",
     [theme.breakpoints.up("md")]: {
       display: "none",
     },
   },
-
-  link: {
-    listStyle: "none",
-    fontSize: "14px",
-    width: "100%",
-    transition: " 0.35s ease",
+  linkContainer: {
+    position: "relative",
+    height: "100px",
+  },
+  icon: {
+    fontSize: "20px",
     color: "#00081a",
+    cursor: "pointer",
+    zIndex: "20",
     [theme.breakpoints.up("md")]: {
       display: "none",
     },
-
-    "&:active": {
-      color: "#00081a",
-    },
-    "&:visited": {
-      color: "#00081a",
-    },
-
-    "&::before": {
-      content: "''",
-      position: "absolute",
-      width: "100%",
-      height: "2px",
-      bottom: "-8px",
-      backgroundColor: "#0091ff",
-      webkitTransform: "scaleX(0)",
-      transform: "scaleX(0)",
-      webkitTransformOrigin: "top left",
-      transformOrigin: "top left",
-      webkitTransition: "-webkit-transform 0.35s ease",
-      transition: "transform 0.35s ease",
-    },
-    "&:hover": {
-      color: "#0091ff",
-      cursor: "pointer",
-      "&::before": {
-        transform: "scale(1)",
-        webkitTransform: "scale(1)",
-      },
-    },
+  },
+  text: {
+    fontSize: "20px",
+    color: "blue",
+    position: "absolute",
+    textAlign: "center",
+    opacity: 1,
+    letterSpacing: "0.1em",
+    transform: "translateY(20px)",
   },
 }))
 
 const MobileNavbar = () => {
   const classes = useStyles()
+
+  const [active, setActive] = useState({
+    activeSection: null,
+    previousActiveSection: null,
+    sections: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }],
+  })
+
+  const handleClick = (num) => {
+    console.log(num, active.activeSection, active.previousActiveSection)
+    if (active?.activeSection?.id === num + 1) {
+      return null
+    } else if (active?.activeSection) {
+      setActive({
+        ...active,
+        previousActiveSection: active.activeSection,
+        activeSection: active.sections[num],
+      })
+    } else {
+      setActive({ ...active, activeSection: active.sections[num] })
+    }
+  }
+
+  const iconsRef = useRef([])
+  const textsRef = useRef([])
+
+  let master = gsap.timeline({
+    repeat: 0,
+    paused: true,
+  })
+
+  const animate = (icon, text) => {
+    let tl = gsap
+      .timeline()
+      .to(icon, {
+        transform: "translateY(-35px)",
+        color: "#0091ff",
+      })
+      .to(
+        text,
+        {
+          autoAlpha: 1,
+          transform: "translateY(0px)",
+        },
+        "-=0.5"
+      )
+  }
+
+  const animateReverse = (icon, text) => {
+    let tl = gsap
+      .timeline()
+      .to(icon, {
+        transform: "translateY(0px)",
+        color: "#00081a",
+      })
+      .to(
+        text,
+        {
+          autoAlpha: 0,
+          transform: "translateY(20px)",
+        },
+        "-=0.5"
+      )
+  }
+
+  switch (active?.activeSection?.id) {
+    case 1:
+      master.add(animate(iconsRef.current[0], textsRef.current[0])).play()
+      break
+    case 2:
+      master.add(animate(iconsRef.current[1], textsRef.current[1])).play()
+      break
+    case 3:
+      master.add(animate(iconsRef.current[2], textsRef.current[2])).play()
+      break
+    case 4:
+      master.add(animate(iconsRef.current[3], textsRef.current[3])).play()
+      break
+  }
+
+  switch (active?.previousActiveSection?.id) {
+    case 1:
+      master
+        .add(animateReverse(iconsRef.current[0], textsRef.current[0]))
+        .play()
+      break
+    case 2:
+      master
+        .add(animateReverse(iconsRef.current[1], textsRef.current[1]))
+        .play()
+      break
+    case 3:
+      master
+        .add(animateReverse(iconsRef.current[2], textsRef.current[2]))
+        .play()
+      break
+    case 4:
+      master
+        .add(animateReverse(iconsRef.current[3], textsRef.current[3]))
+        .play()
+      break
+  }
+
+  /*  active.projects
+    ? master.add(animate(iconsRef.current[0], textsRef.current[0])).play()
+    : master
+        .add(animateReverse(iconsRef.current[0], textsRef.current[0]))
+        .play()
+
+  active.skills
+    ? master.add(animate(iconsRef.current[1], textsRef.current[1])).play()
+    : master
+        .add(animateReverse(iconsRef.current[1], textsRef.current[1]))
+        .play()
+
+  active.about
+    ? master.add(animate(iconsRef.current[2], textsRef.current[2])).play()
+    : master
+        .add(animateReverse(iconsRef.current[2], textsRef.current[2]))
+        .play()
+
+  active.contact
+    ? master.add(animate(iconsRef.current[3], textsRef.current[3])).play()
+    : master
+        .add(animateReverse(iconsRef.current[3], textsRef.current[3]))
+        .play()
+ */
   return (
     <Grid
       container
       item
-      className={classes.linkContainer}
+      className={classes.root}
       direction="row"
       justifyContent="space-around"
       alignItems="center"
     >
-      <Grid item>
-        <li className={classes.link}>
-          <BiArrowToTop />
-        </li>
-      </Grid>
-      <Grid item>
-        <li className={classes.link}>
+      <Grid
+        item
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        sm={2}
+        className={classes.linkContainer}
+      >
+        <div
+          className={classes.icon}
+          ref={(element) => {
+            iconsRef.current[0] = element
+          }}
+          onClick={() => handleClick(0)}
+        >
           <FaProjectDiagram />
-        </li>
+        </div>
+        <div
+          className={classes.text}
+          ref={(element) => {
+            textsRef.current[0] = element
+          }}
+        >
+          Projects
+        </div>
       </Grid>
-      <Grid item>
-        <li className={classes.link}>
+      <Grid
+        item
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        sm={2}
+        className={classes.linkContainer}
+      >
+        <div
+          className={classes.icon}
+          ref={(element) => {
+            iconsRef.current[1] = element
+          }}
+          onClick={() => handleClick(1)}
+        >
           <FaBrain />
-        </li>
+        </div>
+        <div
+          className={classes.text}
+          ref={(element) => {
+            textsRef.current[1] = element
+          }}
+        >
+          {" "}
+          Skills
+        </div>
       </Grid>
-      <Grid item>
-        <li className={classes.link}>
+      <Grid
+        item
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        sm={2}
+        className={classes.linkContainer}
+      >
+        <div
+          className={classes.icon}
+          ref={(element) => {
+            iconsRef.current[2] = element
+          }}
+          onClick={() => handleClick(2)}
+        >
           <FaAddressCard />
-        </li>
+        </div>
+        <div
+          className={classes.text}
+          ref={(element) => {
+            textsRef.current[2] = element
+          }}
+        >
+          About
+        </div>
       </Grid>
-      <Grid item>
-        <li className={classes.link}>
+      <Grid
+        item
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        sm={2}
+        className={classes.linkContainer}
+      >
+        <div
+          className={classes.icon}
+          ref={(element) => {
+            iconsRef.current[3] = element
+          }}
+          onClick={() => handleClick(3)}
+        >
           <FaEnvelope />
-        </li>
+        </div>
+        <div
+          className={classes.text}
+          ref={(element) => {
+            textsRef.current[3] = element
+          }}
+        >
+          Contact
+        </div>
       </Grid>
     </Grid>
   )
